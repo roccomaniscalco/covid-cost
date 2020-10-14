f83603642855de7d3b390aedacc27f5a7839f88e
Garrett Walter <gtwalter150@gmail.com>
2020-10-14 19:55:06 -0400

    created ajax calls for covid api, and iex api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    // iex api
     function stockAPI(){
         var stock = ["APPL","SNAP","IBM"]
         var stocksUrl = "https://cloud.iexapis.com/stable/stock/"+ stock[1] +"/chart/1y/?token=pk_daa92b10b8a84ee7bf59805aa6b96c62";
@@ -33,5 +33,5 @@ $(document).ready(function(){
     stockAPI();
 
     covidAPI();
-    
+
 })
