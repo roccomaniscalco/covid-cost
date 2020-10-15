b01ffa2d1d4eece69ecdfc3c5a959083b4d152b1
Garrett Walter <gtwalter150@gmail.com>
2020-10-14 21:32:12 -0400

    changed how data is displayed into the console

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     // iex api
     function stockAPI(){
-        var stock = ["APPL","SNAP","IBM"]
-        var stocksUrl = "https://cloud.iexapis.com/stable/stock/"+ stock[1] +"/chart/1y/?token=pk_daa92b10b8a84ee7bf59805aa6b96c62";
+        var stock = ["APPL","GUSH","SNAP","IBM"];
+        var range = ["1m","3m","6m","1y"];
+        var stocksUrl = "https://cloud.iexapis.com/stable/stock/"+ stock[1] +"/chart/"+range[2]+"/?token=pk_daa92b10b8a84ee7bf59805aa6b96c62";
             
         $.ajax({
             url: stocksUrl,
@@ -25,7 +26,7 @@ $(document).ready(function(){
             $.ajax(settings).done(function (response) {
                 console.log(response);
                 for(i=0;i<response.length;i++){
-                    console.log("Day " + i + ": "+ response[i].Cases + " Cases");
+                    console.log(response[i].Date + ": "+ response[i].Cases + " Cases");
                 }
             });
     }
