822a5516d5ce8fbf10b73f5782f68b358891a5ae
Brian Shanken <bcshanken@gmail.com>
2020-10-17 10:23:59 -0400

    switched out jquery script from slim to min

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
     
     
     // iex api
-    // function stockAPI(){
+    function stockAPI() {
         var stock = ["APPL","SNAP","IBM"]
         var stocksUrl = "https://cloud.iexapis.com/stable/stock/"+ stock[1] +"/chart/1y/?token=pk_daa92b10b8a84ee7bf59805aa6b96c62";
             
