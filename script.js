bc0bd6d68b414d1fdf3d9347e2bbf41c411b4816
Garrett Walter <gtwalter150@gmail.com>
2020-10-17 11:02:35 -0400

    created functions to display the last 1,3 or 6 months of data, and all data (one year)

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     timePeriod = "";
 
 
+
     // event listener
     $("#stockSubmitBtn").on("click", function(event){
         event.preventDefault()
@@ -39,22 +40,69 @@ $(document).ready(function(){
     }
     
     function covidAPI(){
+        
         var settings = {
             "url": "https://api.covid19api.com/total/country/united-states/status/confirmed?cases",
             "method": "GET",
             "timeout": 0,
         };
     
-            $.ajax(settings).done(function (response) {
-                console.log(response);
-                for(i=0;i<response.length;i++){
-                    console.log(response[i].Date + ": "+ response[i].Cases + " Cases");
+        $.ajax(settings).done(function (response) {
+            // console.log(response);
+
+            function oneMonth(){
+                var month = [];
+                var monthData = [];
+                for (i=1;i<=30;i++){
+                    month.push(response.length -i);
                 }
-            });
+                for (i=0;i<month.length;i++){
+                    monthData.push(response[month[i]].Cases); 
+                }
+                console.log(monthData.reverse());
+            }
+            function threeMonths(){
+                var three = [];
+                var threeData = [];
+                for (i=1;i<=90;i++){
+                    three.push(response.length -i);
+                }
+                for (i=0;i<three.length;i++){
+                    threeData.push(response[three[i]].Cases); 
+                }
+                console.log(threeData.reverse());
+            }
+            function sixMonths(){
+                var six = [];
+                var sixData = [];
+                for (i=1;i<=180;i++){
+                    six.push(response.length -i);
+                }
+                for (i=0;i<six.length;i++){
+                    sixData.push(response[six[i]].Cases); 
+                }
+                console.log(sixData.reverse());
+            }
+            function oneYear(){
+                var year = [];
+                for (i=0;i<response.length;i++){
+                    year.push(response[i].Cases); 
+                }
+                console.log(year);
+            }
+
+            // oneMonth();
+            // threeMonths();
+            // sixMonths();
+            // oneYear();
+               
+        });
     }
 
     stockAPI();
 
     covidAPI();
+
+
 
 })
