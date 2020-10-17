d8c2bafbf8dd6a9de3b1ab503ad07e253f0ea9f2
Dame <dcampbell7095@gmail.com>
2020-10-17 10:53:50 -0400

    adding validation for the stock level option dropdown for the IEX API

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 $(document).ready(function(){
     // JS Variables
 
-    stockLevel = "";
-    stockName = "";
-    timePeriod = "";
+    var stockLevel = "";
+    var stockName = "";
+    var timePeriod = "";
 
 
     // event listener
@@ -13,6 +13,11 @@ $(document).ready(function(){
         stockLevel = $(this).parent().find("#stockPoint").val();
         stockName = $(this).parent().find("#stockInput").val();
         timePeriod = $(this).parent().find("#stockTime").val();
+
+        console.log(stockName)
+        console.log(timePeriod)
+        console.log(stockLevel)
+        stockAPI(timePeriod)
         
     })
     
@@ -22,18 +27,24 @@ $(document).ready(function(){
     
     // iex api
 
-    function stockAPI(){
-        var stock = ["APPL","GUSH","SNAP","IBM"];
-        var range = ["1m","3m","6m","1y"];
-        var stocksUrl = "https://cloud.iexapis.com/stable/stock/"+ stock[1] +"/chart/"+range[2]+"/?token=pk_daa92b10b8a84ee7bf59805aa6b96c62";
+    function stockAPI(userStock){
+
+        var stocksUrl = "https://sandbox.iexapis.com/stable/stock/AAPL/chart/"+userStock+"/?token=Tpk_66abe4f9dd4941ad8eee2c8005748eb3 ";
             
         $.ajax({
             url: stocksUrl,
-            method: "GET"
+            method: "GET",
         }).then(function(response){
             console.log(response);
             for(i=0;i<response.length;i++){
-                console.log(response[i].date +" $"+ response[i].close);
+                // console.log(response[i].date +" $"+ response[i].close);
+                if(stockLevel === "Open"){
+                    console.log(response[i].date + " $" + response[i].open.toString())
+                }else if(stockLevel === "Closing"){
+                    console.log(response[i].date + " $" + response[i].close.toString())
+                }else if(stockLevel === "High"){
+                    console.log(response[i].date + " $" + response[i].high.toString())
+                }
             }
         })
     }
@@ -53,8 +64,8 @@ $(document).ready(function(){
             });
     }
 
-    stockAPI();
+    // stockAPI();
 
-    covidAPI();
+    // covidAPI();
 
 })
